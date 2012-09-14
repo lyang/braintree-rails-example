c4a608665a584bd3cace0b1208f4cf5586acf8dc
Lin Yang <github@linyang.me>
2012-09-13 23:04:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BraintreeRailsExample::Application.config.secret_token = '6dd91361c1b570648f7348f4fce40f6c096c5e16fc0134df5d2c49fb58a94cd2c5a3c14e71453bf25d9eeaf5f61c835e47d3c70c0bf0fc57e7c4fd9cd0ceb7cd'
