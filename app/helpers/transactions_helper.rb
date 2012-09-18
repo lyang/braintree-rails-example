28ce243b551d00e75873d831e050e0df2e8a24fc
Lin Yang <github@linyang.me>
2012-09-17 23:43:36 -0700

    Adding examples of creating non-vault transactions Conflicts: 	Gemfile.lock 	config/routes.rb

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -6,4 +6,16 @@ module TransactionsHelper
       :include_blank => false,
     }
   end
+
+  def new_transaction_path
+    path ||= new_user_customer_credit_card_transaction_path(@user, @credit_card.id) if @credit_card
+    path ||= new_user_customer_transaction_path(@user) if @user
+    path ||= super
+  end
+
+  def back_path
+    path ||= user_customer_credit_card_path(@user, @credit_card.id) if @credit_card
+    path ||= user_customer_path(@user) if @user
+    path ||= users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ BraintreeRailsExample::Application.routes.draw do
       resources :credit_cards do
         member { get :tr_update }
         collection { get :tr_create }
-        resources :transactions, :only => :index
+        resources :transactions, :except => [:edit, :destroy]
       end
       resources :transactions, :except => [:edit, :destroy]
       resources :addresses
     end
   end
-
+  resources :transactions, :except => [:edit, :destroy]
   root :to => 'users#index'
 end
