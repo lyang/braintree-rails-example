8085b57f15299cf32aa2e2921ce246eba928ea51
Lin Yang <github@linyang.me>
2013-03-22 00:06:07 -0700

    Try to make heroku happy with assets compiling

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module BraintreeRailsExample
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
+    config.assets.initialize_on_precompile = false
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
